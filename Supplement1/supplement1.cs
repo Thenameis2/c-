868d4bcc68ef23f16b7c58734588274d42444e56
Mihiretu Jackson <mihiretujackson@gmail.com>
2025-01-30 08:05:56 -0500

    doc: added documentation for area of rectangle

--- Supplement1/supplement1.cs
+++ Supplement1/supplement1.cs
@@ -1,7 +1,14 @@
-﻿namespace Supplement1;
+namespace Supplement1;
 
 public class supplement1 
 {
+/// <summary>
+/// Returns the are of a rectangle. if the length is equal to the width,
+/// the calculation is done as lenght*lenght instead 
+/// </summary>
+/// <param name="length"> lenght of the rectangle </param>
+/// <param name="width"> widht of the rectangle </param>
+/// <returns> Area of the rectange</returns>
     public static double AreaOfRectangle(double length, double width) {
        if(length == width){
         return length * length;
