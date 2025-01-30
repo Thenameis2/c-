f74e471e86d154429484ba25dad7f21d4acadaf9
Mihiretu Jackson <mihiretujackson@gmail.com>
2025-01-30 07:26:38 -0500

    chore: decared methods for are calcualtion

--- Supplement1/supplement1.cs
+++ Supplement1/supplement1.cs
@@ -1,6 +1,19 @@
 ﻿namespace Supplement1;
 
-public class supplement1
+public class supplement1 
 {
+    public static double AreaOfRectangle(double length, double width) {
+        throw new NotImplementedException();
+    }
+    public static double AreaOfCircle(double radius) {
+        throw new NotImplementedException();
+    }.
+    
+    public static double AreaOfTriangle(double triangleBase, double triangleHeight){
+        
+        throw new NotImplementedException;
+        }
 
 }
+
+
