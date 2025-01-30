75a6c4da5703cc5339d492337b153ecc8abcf317
Mihiretu Jackson <mihiretujackson@gmail.com>
2025-01-30 08:15:21 -0500

    test: area of circle, text for unit circle

--- Supplement1.Tests/UnitTest1.cs
+++ Supplement1.Tests/UnitTest1.cs
@@ -16,4 +16,11 @@ public void ShouldCalculateArea0fRectangleWithSameLengthAndWidth()
   Assert.True(25.0 == supplement1.AreaOfRectangle(5.0, 5.0));
   
   }
+
+  public void ShouldCalculateArea0fUnitCircle()
+  {
+
+  Assert.True(double.Pi == supplement1.AreaOfCircle(1.0));
+  
+  }
 }
