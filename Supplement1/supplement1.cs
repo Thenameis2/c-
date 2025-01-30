94ae39715d63045046cd35565276f77cb770ba55
Mihiretu Jackson <mihiretujackson@gmail.com>
2025-01-30 08:36:01 -0500

    doc: added documentation for area of Triangle

--- Supplement1/supplement1.cs
+++ Supplement1/supplement1.cs
@@ -20,7 +20,7 @@ public class supplement1
     }
 
     /// <summary>
-    /// Returns the area of the circle
+    /// Returns the area of a circle
     /// </summary>
     /// <param name="radius"> Radius of a circle</param>
     /// <returns> The radius of a circle with a given radius.</returns>
@@ -28,6 +28,12 @@ public class supplement1
        return double.Pow(radius,2.0)* double.Pi;
     }
     
+    /// <summary>
+    /// Returns the area of a triangle.
+    /// </summary>
+    /// <param name="triangleBase"> Base of the triangl e</param>
+    /// <param name="triangleHeight"> Height of the triangle </param>
+    /// <returns>area of the triangle with the given base and height</returns>
     public static double AreaOfTriangle(double triangleBase, double triangleHeight){
         
        return triangleBase * triangleHeight / 2;
