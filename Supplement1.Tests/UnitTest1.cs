2b5987a687c39bd0ee8c596e00d30a00acebaa55
Mihiretu Jackson <mihiretujackson@gmail.com>
2025-01-30 07:48:55 -0500

    feature: implemenetd base case

--- Supplement1.Tests/UnitTest1.cs
+++ Supplement1.Tests/UnitTest1.cs
@@ -6,8 +6,6 @@ public class UnitTest1
     
 public void ShouldCalculateAreaOfRectangleWithDifferentLengthAndWidth()
 {
-
-
-Assert.True(50.0 == supplement_p1_1.AreafRectangle(5.0, 10.0));
+    Assert.True(50.0 == supplement1.AreaOfRectangle(5.0, 10.0));
 }
 }
--- Supplement1/supplement1.cs
+++ Supplement1/supplement1.cs
@@ -3,7 +3,7 @@
 public class supplement1 
 {
     public static double AreaOfRectangle(double length, double width) {
-        throw new NotImplementedException();
+       return length * width;
     }
     public static double AreaOfCircle(double radius) {
         throw new NotImplementedException();
@@ -11,8 +11,8 @@ public class supplement1
     
     public static double AreaOfTriangle(double triangleBase, double triangleHeight){
         
-        throw new NotImplementedException;
-        }
+        throw new NotImplementedException();
+    }
 
 }
 
